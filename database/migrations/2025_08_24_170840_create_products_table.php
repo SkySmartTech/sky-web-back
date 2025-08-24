816a0d59d520deeec91529f80c859996341f1e2d
Sheshan-zero <sheshandealwis20021@gmail.com>
2025-08-24 22:44:24 +0530

    feat(products): add categories and products tables + models

---
+++ database/migrations/2025_08_24_170840_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id')
+                  ->constrained('categories')
+                  ->cascadeOnUpdate()
+                  ->restrictOnDelete();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('description')->nullable();
+            $table->decimal('price', 10, 2)->nullable();
+            $table->string('image')->nullable();     
+            $table->string('thumbnail')->nullable();
+            $table->string('url')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
